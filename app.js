5fb4054edbaf765dc3ecb194855f31c304f4aeb9
salma <salma.hossam.harb@gmail.com>
2023-12-26 20:23:14 +0200

    Add router to tasks

--- app.js
+++ app.js
@@ -6,27 +6,12 @@ const mongoose = require('mongoose');
 const models = require('./models')
 var indexRouter = require('./routes/index');
 var eventsRouters = require('./routes/events');
+var tasksRouter = require('./routes/tasks');
 
 const cors = require('cors')
 
 mongoose.connect('mongodb://salHarb:salma1234@ac-pi2hqji-shard-00-00.yrxuuqu.mongodb.net:27017,ac-pi2hqji-shard-00-01.yrxuuqu.mongodb.net:27017,ac-pi2hqji-shard-00-02.yrxuuqu.mongodb.net:27017/?ssl=true&replicaSet=atlas-4wzwve-shard-0&authSource=admin&retryWrites=true&w=majority', {}).then(async connection => {
   console.log("Connected");
-
-  // const user1 = new models.users({
-  //   name: "Salma",
-  //   email: "salma@gmail.com",
-  //   password: "abcd1234",
-  //   avatar: '1.png'
-  // })
-
-  // await user1.save()
-  // const event1 = new models.events({
-  //   date: new Date("2023-01-01"),
-  //   name: 'First event',
-  //   desc: "Description 1",
-  //   user: user1
-  // })
-  // await event1.save()
 })
 
 
@@ -39,6 +24,8 @@ app.use(express.json());
 app.use(cors())
 app.use('/', indexRouter);
 app.use('/events', eventsRouters);
+app.use('/tasks', tasksRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
